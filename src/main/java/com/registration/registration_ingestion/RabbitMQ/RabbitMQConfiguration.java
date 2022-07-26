29612d613f10c152218693457b56282ac4884659
USER <1905707@kiit.ac.in>
2022-07-27 01:27:32 +0530

    Modified to remove accepting the values from properties file...

--- src/main/java/com/registration/registration_ingestion/RabbitMQ/RabbitMQConfiguration.java
+++ src/main/java/com/registration/registration_ingestion/RabbitMQ/RabbitMQConfiguration.java
@@ -1,24 +1,15 @@
 package com.registration.registration_ingestion.RabbitMQ;
 
 import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-class RabbitMQConfiguration {
-
-    @Value("${user_verification_queue_name}")
-    private String QUEUE_NAME;
-    @Value("${user_verification_exchange}")
-    private String EXCHANGE_NAME;
-    @Value("${user_verification_routing_key}")
-    private String ROUTING_KEY;
+class RabbitMQConfiguration implements RabbitConfigConstants {
 
     @Bean
     String getEXCHANGE_NAME(){
@@ -41,10 +32,10 @@ class RabbitMQConfiguration {
     }
 
     @Bean
-    public Binding binding(Queue queue, TopicExchange topicExchange){
+    public Binding binding(){
         return BindingBuilder
-                .bind(queue)
-                .to(topicExchange)
+                .bind(queue())
+                .to(topicExchange())
                 .with(ROUTING_KEY);
     }
 
