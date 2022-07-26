b97f0223d5ddb2a8e266284ec21ca7219cb7269b
USER <1905707@kiit.ac.in>
2022-07-26 22:50:05 +0530

    Modified to remove default method...

--- src/main/java/com/registration/registration_ingestion/Services/IngestionService.java
+++ src/main/java/com/registration/registration_ingestion/Services/IngestionService.java
@@ -3,10 +3,6 @@ package com.registration.registration_ingestion.Services;
 import com.registration.registration_ingestion.Models.IngestionData.User;
 
 public interface IngestionService {
-    default boolean verifyEmailDomainAndPattern(String email){
-        return false;
-    }
-    default boolean enqueNewUserCreationRequest(User NewUser){
-        return false;
-    }
+    boolean verifyEmailDomainAndPattern(String email);
+    boolean enqueNewUserCreationRequest(User newUserToVerify);
 }
