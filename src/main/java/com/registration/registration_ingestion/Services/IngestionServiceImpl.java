df0309c48ef10931ac10d716459ef069d085aab4
USER <1905707@kiit.ac.in>
2022-08-19 01:23:16 +0530

    Implemented 'getSessionID' method and modified the 'verifyEmailDomainAndPattern' method to assign a default value of the regex pattern is empty

--- src/main/java/com/registration/registration_ingestion/Services/IngestionServiceImpl.java
+++ src/main/java/com/registration/registration_ingestion/Services/IngestionServiceImpl.java
@@ -1,11 +1,13 @@
 package com.registration.registration_ingestion.Services;
 
-import com.registration.registration_ingestion.Models.IngestionData.User;
+import com.registration.registration_ingestion.Models.RabbitMessage;
 import com.registration.registration_ingestion.RabbitMQ.RabbitMessageProducer;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.regex.Pattern;
 
@@ -15,13 +17,28 @@ final class IngestionServiceImpl implements IngestionService
     @Autowired
     private RabbitMessageProducer rabbitMessageProducer;
 
+    @Value("${values.email_name_pattern_regex}")
+    private String email_nameBox_regex;
+
     @Override
-    public boolean enqueNewUserCreationRequest(User newUserToVerify) {
+    public boolean enqueNewUserCreationRequest(RabbitMessage newUserToVerify) {
         return rabbitMessageProducer.EnqueueUserForVerification(newUserToVerify);
     }
 
+    @Override
+    public String getSessionID() {
+        return UUID.randomUUID().toString();
+    }
+
     public boolean verifyEmailDomainAndPattern(String email) {
-        Pattern emailPattern = Pattern.compile("\\d*");
+        /**
+         * sets a default value for the pattern of email name
+         * if the value is not provided through the properties file.
+         * */
+        if(email_nameBox_regex == null || email_nameBox_regex.compareToIgnoreCase("")==0)
+            email_nameBox_regex = "[[a-z]+[A-z]]*";
+        final Pattern emailPattern = Pattern.compile(email_nameBox_regex);
+
         AtomicBoolean isMailboxNameOk = new AtomicBoolean(false);
         AtomicBoolean isDomainNameOk = new AtomicBoolean(false);
 
