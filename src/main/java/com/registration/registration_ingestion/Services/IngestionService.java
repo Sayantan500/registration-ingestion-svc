5fda56f28ea64aa014dd49b0289b475e3eb5306f
USER <1905707@kiit.ac.in>
2022-08-19 01:19:06 +0530

    Changed the message type for rabbitmq

--- src/main/java/com/registration/registration_ingestion/Services/IngestionService.java
+++ src/main/java/com/registration/registration_ingestion/Services/IngestionService.java
@@ -1,8 +1,9 @@
 package com.registration.registration_ingestion.Services;
 
-import com.registration.registration_ingestion.Models.IngestionData.User;
+import com.registration.registration_ingestion.Models.RabbitMessage;
 
 public interface IngestionService {
     boolean verifyEmailDomainAndPattern(String email);
-    boolean enqueNewUserCreationRequest(User newUserToVerify);
+    boolean enqueNewUserCreationRequest(RabbitMessage newUserToVerify);
+    String getSessionID();
 }
