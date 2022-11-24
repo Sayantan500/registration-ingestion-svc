2479be98954635a75732fd43ebe38d5c0318cf95
USER <1905707@kiit.ac.in>
2022-11-25 01:02:01 +0530

    Added method to check if the user is already registered...

--- src/main/java/com/registration/registration_ingestion/Services/IngestionService.java
+++ src/main/java/com/registration/registration_ingestion/Services/IngestionService.java
@@ -3,6 +3,7 @@ package com.registration.registration_ingestion.Services;
 import com.registration.registration_ingestion.Models.RabbitMessage;
 
 public interface IngestionService {
+    boolean isUserAlreadyPresent(String email, String password);
     boolean verifyEmailDomainAndPattern(String email);
     boolean enqueNewUserCreationRequest(RabbitMessage newUserToVerify);
     String getSessionID();
