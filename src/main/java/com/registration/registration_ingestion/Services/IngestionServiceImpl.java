8238846d31456d3cc2e5b76de8af1e2a88a8ba38
USER <1905707@kiit.ac.in>
2022-11-25 01:01:27 +0530

    Added method to check if the user is already registered...and modified field injection to constructor injection...

--- src/main/java/com/registration/registration_ingestion/Services/IngestionServiceImpl.java
+++ src/main/java/com/registration/registration_ingestion/Services/IngestionServiceImpl.java
@@ -1,11 +1,15 @@
 package com.registration.registration_ingestion.Services;
 
+import com.registration.registration_ingestion.Models.LoginRequestBody;
 import com.registration.registration_ingestion.Models.RabbitMessage;
 import com.registration.registration_ingestion.RabbitMQ.RabbitMessageProducer;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -14,11 +18,28 @@ import java.util.regex.Pattern;
 @Service
 final class IngestionServiceImpl implements IngestionService
 {
-    @Autowired
-    private RabbitMessageProducer rabbitMessageProducer;
+    private final RabbitMessageProducer rabbitMessageProducer;
+    private final RestTemplate restTemplate;
+    private final String baseUrlLogin;
+    private final String email_nameBox_regex;
 
-    @Value("${values.email_name_pattern_regex}")
-    private String email_nameBox_regex;
+    @Autowired
+    public IngestionServiceImpl(
+            RabbitMessageProducer rabbitMessageProducer,
+            RestTemplate restTemplate,
+            @Qualifier("login_base_url") String baseUrlLogin,
+            @Qualifier("email_name_pattern") String email_nameBox_regex
+    )
+    {
+        this.rabbitMessageProducer = rabbitMessageProducer;
+        this.restTemplate = restTemplate;
+        this.baseUrlLogin = baseUrlLogin;
+
+        if(email_nameBox_regex == null || email_nameBox_regex.compareToIgnoreCase("")==0)
+            this.email_nameBox_regex = "[[a-z]+[A-z]]*";
+        else
+            this.email_nameBox_regex = email_nameBox_regex;
+    }
 
     @Override
     public boolean enqueNewUserCreationRequest(RabbitMessage newUserToVerify) {
@@ -30,13 +51,33 @@ final class IngestionServiceImpl implements IngestionService
         return UUID.randomUUID().toString();
     }
 
+    @Override
+    public boolean isUserAlreadyPresent(String email, String password) {
+        String requestUrl = baseUrlLogin + "/signin";
+        try
+        {
+            restTemplate.exchange(
+                    requestUrl,
+                    HttpMethod.POST,
+                    new HttpEntity<>(new LoginRequestBody(email,password)),
+                    Object.class,
+                    1
+            );
+        }
+        catch (RuntimeException exception)
+        {
+            return true; //means any 4xx error code is received implying user is not present.
+        }
+        return false;
+    }
+
     public boolean verifyEmailDomainAndPattern(String email) {
         /*
           sets a default value for the pattern of email name
           if the value is not provided through the properties file.
           */
-        if(email_nameBox_regex == null || email_nameBox_regex.compareToIgnoreCase("")==0)
-            email_nameBox_regex = "[[a-z]+[A-z]]*";
+        /*if(email_nameBox_regex == null || email_nameBox_regex.compareToIgnoreCase("")==0)
+            email_nameBox_regex = "[[a-z]+[A-z]]*";*/
         final Pattern emailPattern = Pattern.compile(email_nameBox_regex);
 
         AtomicBoolean isMailboxNameOk = new AtomicBoolean(false);
