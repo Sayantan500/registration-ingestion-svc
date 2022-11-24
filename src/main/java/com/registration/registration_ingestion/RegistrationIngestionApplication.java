fbcca8c20788891b74597bef4c538ec53f711f0f
USER <1905707@kiit.ac.in>
2022-11-25 00:58:52 +0530

    Added beans for base url, regex pattern and rest template...

--- src/main/java/com/registration/registration_ingestion/RegistrationIngestionApplication.java
+++ src/main/java/com/registration/registration_ingestion/RegistrationIngestionApplication.java
@@ -1,8 +1,11 @@
 package com.registration.registration_ingestion;
 
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableRabbit
@@ -10,6 +13,27 @@ public class RegistrationIngestionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RegistrationIngestionApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
+
+	@Value("${baseurl.signin}")
+	private String baseUrlLogin;
+
+	@Bean(name = "login_base_url")
+	public String getBaseUrlLogin(){
+		return baseUrlLogin;
+	}
+
+	@Value("${values.email_name_pattern_regex}")
+	private String email_nameBox_regex;
+
+	@Bean(name = "email_name_pattern")
+	public String getEmail_nameBox_regex(){
+		return email_nameBox_regex;
 	}
 
 }
